5b6ee79201bd2f5c005c13d396701de6406e022a
William <williampmb@gmail.com>
2021-03-18 09:57:52 -0300

    First commit

---
+++ src/main/java/com/login/service/mycourse/MycourseApplication.java
@@ -0,0 +1,13 @@
+package com.login.service.mycourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/login/service/mycourse/MycourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.login.service.mycourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
